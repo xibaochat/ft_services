92cb9f5de86905d71b8ddf0207d3131b84b55200
xibaochat <boddez.wang.xin@protonmail.com>
2020-07-06 23:35:52 +0200

    hao

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -2,6 +2,9 @@ kubectl apply -f mysql/volume/mysql_pv.yaml
 
 kubectl apply -f mysql/volume/mysql_pvc.yaml
 
+BAOBE_COM_SVC_IP=$(kubectl get svc baobe-service-http -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+sed -i'.original' -e 's/localhost/'"${BAOBE_COM_SVC_IP}"'/g' mysql/backup/wordpress_tables.sql
+
 docker build mysql/backup -t mysql_backup:00
 
 kubectl apply -f mysql/backup/mysql_backup_pod.yaml
