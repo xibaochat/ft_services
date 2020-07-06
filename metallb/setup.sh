3e26034bd8ec39d7c288efe509c0267d47ebf455
xibaochat <boddez.wang.xin@protonmail.com>
2020-07-06 12:48:36 +0200

    change metallb

--- metallb/setup.sh
+++ metallb/setup.sh
@@ -1,6 +1,5 @@
-kubectl create ns metallb-system
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
+BASE_IP=$(minikube ip | cut -d '.' -f 1-3)
+
+sed -i'.original' -e's/BASE_IP/'"${BASE_IP}"'/g' configMap.yaml
+
 kubectl apply -f configMap.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" -o yaml --dry-run=client > metallb-secret.yaml
-kubectl apply -f metallb-secret.yaml
-rm -f metallb-secret.yaml
