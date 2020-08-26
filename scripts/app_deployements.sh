b5eae5cdbd1223706dce01306667af5933ddd841
xibaochat <boddez.wang.xin@protonmail.com>
2020-08-26 21:08:24 +0200

    remove #

--- scripts/app_deployements.sh
+++ scripts/app_deployements.sh
@@ -3,11 +3,11 @@ exec_sh()
 	$@/setup.sh
 }
 
-#exec_sh nginx_ssh
-#exec_sh ftps
-#exec_sh baobe_com
-#exec_sh mysql
-#exec_sh phpmyadmin
+exec_sh nginx_ssh
+exec_sh ftps
+exec_sh baobe_com
+exec_sh mysql
+exec_sh phpmyadmin
 exec_sh influxdb
 exec_sh telegraf
 exec_sh grafana
