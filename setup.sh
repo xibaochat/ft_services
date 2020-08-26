68939bedffafb92e4c466b318d8d6d9d0fa5f9be
xibaochat <boddez.wang.xin@protonmail.com>
2020-08-26 21:19:18 +0200

    change #

--- setup.sh
+++ setup.sh
@@ -8,14 +8,16 @@ source $OUTPUT_FUNCS
 simple_output "Starting ${BLUE}ft_services${NC} setup"
 
 simple_output "Installing binaries"
-#$SCRIPTS_PATH/binaries_installation.sh
+$SCRIPTS_PATH/binaries_installation.sh
 
 simple_output "Installing addons"
-#$SCRIPTS_PATH/addons_installation.sh
+$SCRIPTS_PATH/addons_installation.sh
 
 eval $(minikube docker-env)
 kubectl delete configmaps,deploy,svc,job,pods,pv,pvc --all
 
+#create conf
+kubectl create configmap telegraf-config --from-file=telegraf/telegraf.toml
 kubectl create configmap grafana-config --from-file=influxdb-datasource.yaml=grafana/config/influxdb-datasource.yaml --from-file=dashboards.yaml=grafana/config/dashboards.yaml --from-file=analysis-server.json=grafana/config/analysis-server.json
 simple_output "Deploying applications"
 
