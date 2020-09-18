45c9f569ae3df3f9a1758709c99f38045da8d0b4
xibaochat <boddez.wang.xin@protonmail.com>
2020-09-18 22:50:30 +0200

    nginx redirect

--- nginx_ssh/setup.sh
+++ nginx_ssh/setup.sh
@@ -1,7 +1,11 @@
 echo $PWD
 
+kubectl apply -f nginx_ssh/manifest/nginx_service.yaml
+
+sleep 10
+
+LOADBALANCER_IP=`kubectl get services  nginx --output jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+sed -i'.original' -e 's|LOADBALANCER_IP|'"$LOADBALANCER_IP"'|g' nginx_ssh/src/my_nginx.conf
 docker build nginx_ssh -t  nginx_ssh:00
 
 kubectl apply -f nginx_ssh/manifest/nginx_deploy.yaml
-
-kubectl apply -f nginx_ssh/manifest/nginx_service.yaml
