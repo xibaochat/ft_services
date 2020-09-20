192ee1fc5f5a9be6b30657d9d90d8c4366b0174f
xibaochat <boddez.wang.xin@protonmail.com>
2020-09-20 18:11:29 +0200

    Fix platform dependent sed cmd

--- metallb/setup.sh
+++ metallb/setup.sh
@@ -1,5 +1,8 @@
 BASE_IP=$(minikube ip | cut -d '.' -f 1-3)
 
-sed -i'.original' -e's/BASE_IP/'"${BASE_IP}"'/g' metallb/configMap.yaml
+case "$(uname -s)" in
+    Linux*)     sed -i 's/BASE_IP/'"${BASE_IP}"'/g' metallb/configMap.yaml;;
+    Darwin*)    sed -i'.original' -e's/BASE_IP/'"${BASE_IP}"'/g' metallb/configMap.yaml;;
+esac
 
 kubectl apply -f metallb/configMap.yaml
