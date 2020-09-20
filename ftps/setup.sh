76b3f6cf97fadecc78babf355c7120db364fc9de
xibaochat <boddez.wang.xin@protonmail.com>
2020-09-20 18:07:31 +0200

    Put sed into dockerfile

--- ftps/setup.sh
+++ ftps/setup.sh
@@ -3,7 +3,6 @@ kubectl apply -f  ftps/manifest/ftps_service.yaml
 sleep 10
 
 LOADBALANCER_IP=`kubectl get services  ftps-service --output jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-sed -i'.original' -e 's|LOADBALANCER_IP|'"$LOADBALANCER_IP"'|g' ftps/src/ftps_conf
 
-docker build ftps/ -t ftps:00
+docker build --build-arg LOADBALANCER_IP=$LOADBALANCER_IP ftps/ -t ftps:00
 kubectl apply -f  ftps/manifest/ftps_deploy.yaml
