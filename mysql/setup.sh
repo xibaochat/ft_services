91de73b73b4296d1267224ecc4f09ae714642b0e
xibaochat <boddez.wang.xin@protonmail.com>
2020-09-20 21:18:46 +0200

    Fix baobe.com port

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -3,7 +3,7 @@ kubectl apply -f mysql/volume/mysql_pv.yaml
 kubectl apply -f mysql/volume/mysql_pvc.yaml
 
 BAOBE_COM_SVC_IP=$(kubectl get svc baobe-service-http -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-sed -i'.original' -e 's/localhost/'"${BAOBE_COM_SVC_IP}"'/g' mysql/backup/wordpress_tables.sql
+sed -i'.original' -e 's/localhost/'"${BAOBE_COM_SVC_IP}:5050"'/g' mysql/backup/wordpress_tables.sql
 
 docker build mysql/backup -t mysql_backup:00
 
