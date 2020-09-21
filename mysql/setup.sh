277bcd44ecddf098856844e1c441fdce341ba2ba
xibaochat <boddez.wang.xin@protonmail.com>
2020-09-21 15:27:27 +0200

    nginx LOADBALANCER_IP

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -3,9 +3,8 @@ kubectl apply -f mysql/volume/mysql_pv.yaml
 kubectl apply -f mysql/volume/mysql_pvc.yaml
 
 BAOBE_COM_SVC_IP=$(kubectl get svc baobe-service-http -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-sed -i'.original' -e 's/localhost/'"${BAOBE_COM_SVC_IP}:5050"'/g' mysql/backup/wordpress_tables.sql
 
-docker build mysql/backup -t mysql_backup:00
+docker build --build-arg BAOBE_COM_SVC_IP=$BAOBE_COM_SVC_IP mysql/backup -t mysql_backup:00
 
 kubectl apply -f mysql/backup/mysql_backup_pod.yaml
 
--- nginx_ssh/setup.sh
+++ nginx_ssh/setup.sh
@@ -5,7 +5,7 @@ kubectl apply -f nginx_ssh/manifest/nginx_service.yaml
 sleep 10
 
 LOADBALANCER_IP=`kubectl get services  nginx --output jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-sed -i'.original' -e 's|LOADBALANCER_IP|'"$LOADBALANCER_IP"'|g' nginx_ssh/src/my_nginx.conf
-docker build nginx_ssh -t  nginx_ssh:00
+
+docker build --build-arg LOADBALANCER_IP=$LOADBALANCER_IP nginx_ssh -t  nginx_ssh:00
 
 kubectl apply -f nginx_ssh/manifest/nginx_deploy.yaml
